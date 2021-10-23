638edfeacc7a00c56d2c585396ee63569d3d98a4
Pablo Muralles <pmmuralles@correo.url.edu.gt>
2021-10-23 11:51:59 -0600

    querys de llenado

--- QueriesLlenado.sql
+++ QueriesLlenado.sql
@@ -1,106 +1,96 @@
 --Queries para llenar datos
 
 --Dimensiones
+	--DimCliente
+	select C.ID_Cliente,
+			C.PrimerNombre,
+			C.SegundoNombre,
+			C.PrimerApellido,
+			C.SegundoApellido,
+			C.Genero,
+			C.Correo_Electronico,
+			C.FechaNacimiento,
+			C.Direccion
+			--Columnas Auditoria
+			,GETDATE() AS FechaCreacion
+			,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioCreacion
+			,GETDATE() AS FechaModificacion
+			,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioModificacion
+			,CAST('2020-01-01' AS DATETIME) as FechaInicioValidez
+	from DBO.Clientes C
 
-	--DimCarrera
-	INSERT INTO Dimension.Carrera
-	(ID_Carrera, 
-	 ID_Facultad, 
-	 NombreCarrera, 
-	 NombreFacultad
-	)
-	SELECT C.ID_Carrera, 
-			F.ID_Facultad, 
-			C.Nombre, 
-			F.Nombre
-	FROM Admisiones.dbo.Facultad F
-		INNER JOIN Admisiones.dbo.Carrera C ON(C.ID_Facultad = F.ID_Facultad);
-	
-	SELECT * FROM Dimension.Carrera
+	--DimGeografia
+	SELECT c.ID_Ciudad,
+			c.ID_Region,
+			r.ID_Pais,
+			c.Nombre as NombreCiudad,
+			c.CodigoPostal,
+			r.Nombre as NombreRegion,
+			p.Nombre as NombrePais
+			--Columnas Auditoria
+			,GETDATE() AS FechaCreacion
+			,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioCreacion
+			,GETDATE() AS FechaModificacion
+			,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioModificacion
+			,CAST('2020-01-01' AS DATETIME) as FechaInicioValidez
+	  FROM dbo.Ciudad c inner join dbo.Region r on (c.ID_Region = c.ID_Region)
+	  inner join dbo.Pais p on (r.ID_Pais = p.ID_Pais)
 
-	--DimCandidato
-	INSERT INTO Dimension.Candidato
-	([ID_Candidato], 
-	 [ID_Colegio], 
-	 [ID_Diversificado], 
-	 [NombreCandidato], 
-	 [ApellidoCandidato], 
-	 [Genero], 
-	 [FechaNacimiento], 
-	 [NombreColegio], 
-	 [NombreDiversificado]
-	)
-	SELECT C.ID_Candidato, 
-			CC.ID_Colegio, 
-			D.ID_Diversificado, 
-			C.Nombre as NombreCandidato, 
-			C.Apellido as ApellidoCandidato, 
-			C.Genero, 
-			C.FechaNacimiento, 
-			CC.Nombre as NombreColegio, 
-			D.Nombre as NombreDiversificado
-	FROM Admisiones.DBO.Candidato C
-		INNER JOIN Admisiones.DBO.ColegioCandidato CC ON(C.ID_Colegio = CC.ID_Colegio)
-		INNER JOIN Admisiones.DBO.Diversificado D ON(C.ID_Diversificado = D.ID_Diversificado);
+	  --DimVehiculos
+	   SELECT V.VehiculoID,
+				V.VIN_Patron,
+				V.Anio,
+				V.Marca,
+				V.Modelo,
+				V.SubModelo,
+				V.Estilo,
+				V.FechaCreacion
+				--Columnas Auditoria
+				,GETDATE() AS FechaCreacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioCreacion
+				,GETDATE() AS FechaModificacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioModificacion
+				,CAST('2020-01-01' AS DATETIME) as FechaInicioValidez
+		FROM dbo.Vehiculo
 
-		SELECT * FROM Dimension.Candidato
+		--DimPartes
+		SELECT P.ID_Partes,
+				L.ID_Linea, C.ID_Categoria, 
+				P.Nombre, P.Descripcion, 
+				P.Precio, C.Nombre, 
+				C.Descripcion, 
+				L.Nombre, 
+				L.Descripcion
+				--Columnas Auditoria
+				,GETDATE() AS FechaCreacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioCreacion
+				,GETDATE() AS FechaModificacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioModificacion
+				,CAST('2020-01-01' AS DATETIME) as FechaInicioValidez
+		FROM dbo.Partes AS P
+		INNER JOIN dbo.Categoria AS C ON P.ID_Categoria = C.ID_Categoria
+		INNER JOIN dbo.Linea AS L ON C.ID_Linea = L.ID_Linea
 
---------------------------------------------------------------------------------------------
------------------------CORRER CREATE de USP_FillDimDate PRIMERO!!!--------------------------
---------------------------------------------------------------------------------------------
+		--DimStatus
+		SELECT s.ID_StatusOrden, 
+				s.NombreStatus
+				--Columnas Auditoria
+				,GETDATE() AS FechaCreacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioCreacion
+				,GETDATE() AS FechaModificacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioModificacion
+				,CAST('2020-01-01' AS DATETIME) as FechaInicioValidez
+		FROM dbo.StatusOrden s
 
-	DECLARE @FechaMaxima DATETIME=DATEADD(YEAR,2,GETDATE())
-	--Fecha
-	IF ISNULL((SELECT MAX(Date) FROM Dimension.Fecha),'1900-01-01')<@FechaMaxima
-	begin
-		EXEC USP_FillDimDate @CurrentDate = '2016-01-01', 
-							 @EndDate     = @FechaMaxima
-	end
-	SELECT * FROM Dimension.Fecha
-	
-	--Fact
-	INSERT INTO [Fact].[Examen]
-	([SK_Candidato], 
-	 [SK_Carrera], 
-	 [DateKey], 
-	 [ID_Examen], 
-	 [ID_Descuento], 	
-	 [DescripcionDescuento], 
-	 [PorcentajeDescuento], 
-	 [Precio], 
-	 [NotaTotal], 
-	 [NotaArea], 
-	 [NombreMateria]
-	)
-	SELECT  --Columnas de mis dimensiones en DWH
-			SK_Candidato, 
-			SK_Carrera, 
-			F.DateKey, 
-			R.ID_Examen, 
-			R.ID_Descuento, 			
-			D.Descripcion, 
-			D.PorcentajeDescuento, 
-			R.Precio, 
-			R.Nota,
-			RR.NotaArea, 
-			EA.NombreMateria
-				 
-	FROM Admisiones.DBO.Examen R
-		INNER JOIN Admisiones.DBO.Examen_Detalle RR ON(R.ID_Examen = RR.ID_Examen)
-		INNER JOIN Admisiones.DBO.Materia EA ON(EA.ID_Materia = RR.ID_Materia)
-		INNER JOIN Admisiones.DBO.Descuento D ON(D.ID_Descuento = R.ID_Descuento)
-		--Referencias a DWH
-		INNER JOIN Dimension.Candidato C ON(C.ID_Candidato = R.ID_Candidato)
-		INNER JOIN Dimension.Carrera CA ON(CA.ID_Carrera = R.ID_Carrera)
-		INNER JOIN Dimension.Fecha F ON(CAST((CAST(YEAR(R.FechaPrueba) AS VARCHAR(4)))+left('0'+CAST(MONTH(R.FechaPrueba) AS VARCHAR(4)),2)+left('0'+(CAST(DAY(R.FechaPrueba) AS VARCHAR(4))),2) AS INT)  = F.DateKey);
+		--DimDescuento
+		SELECT ID_Descuento,
+				NombreDescuento,
+				PorcentajeDescuento
+				--Columnas Auditoria
+				,GETDATE() AS FechaCreacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioCreacion
+				,GETDATE() AS FechaModificacion
+				,CAST(SUSER_NAME() AS nvarchar(100)) AS UsuarioModificacion
+				,CAST('2020-01-01' AS DATETIME) as FechaInicioValidez
+		 FROM dbo.Descuento
 
-
---------------------------------------------------------------------------------------------
-------------------------------------Resultado Final-----------------------------------------
---------------------------------------------------------------------------------------------	
-
-	SELECT *
-	FROM	Fact.Examen AS E INNER JOIN
-			Dimension.Candidato AS C ON E.SK_Candidato = C.SK_Candidato INNER JOIN
-			Dimension.Carrera AS CA ON E.SK_Carrera = CA.SK_Carrera INNER JOIN
-			Dimension.Fecha AS F ON E.DateKey = F.DateKey
